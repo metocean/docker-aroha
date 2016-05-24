36e6e891b94b21bac7894f91b3475e82f2a979f4
Greg Chalmers <g.chalmers@metocean.co.nz>
2016-05-24 12:55:09 +1200

    stopped building consul, as it fails to build

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM gliderlabs/alpine:3.3
 MAINTAINER Thomas Coats <thomas@metocean.co.nz>
 
-ENV CONSUL_VERSION=0.6.4 GOMAXPROCS=2
+ENV CONSUL_VERSION=0.6.4
 
 ADD . /install/
 RUN /install/install.sh
--- install.sh
+++ install.sh
@@ -8,15 +8,14 @@ edge_community_repo="http://dl-4.alpinelinux.org/alpine/edge/community"
 
 echo "$edge_community_repo" >> /etc/apk/repositories
 apk update
-apk add go git gcc make musl-dev bash unzip zip tar curl
+apk add git gcc make musl-dev bash unzip zip tar curl
 
 echo '----- Installing Consul -----'
-export GOPATH=/tmp/go
-go get github.com/hashicorp/consul
-cd /tmp/go/src/github.com/hashicorp/consul
-git checkout v$CONSUL_VERSION
-make
-mv bin/consul /usr/bin
+cd /tmp
+curl -o consul.zip -L https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+unzip consul.zip
+chmod +x consul
+mv consul /usr/bin
 
 echo '----- Installing Consul UI -----'
 cd /tmp
@@ -37,7 +36,7 @@ make
 cp chpst runit runit-init runsv runsvchdir runsvdir sv svlogd utmpset /sbin
 
 echo '----- cleaning up apk packages -----'
-apk del go git gcc make musl-dev bash unzip zip tar curl
+apk del git gcc make musl-dev bash unzip zip tar curl
 echo "$(grep -v "$edge_community_repo" /etc/apk/repositories)" > /etc/apk/repositories
 
 # Install node.js syslog-ng zeromq initsh bashalias
